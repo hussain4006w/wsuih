ec7e37f248982a3025306c414c6dbc9037c24db4
hussain <130484336+hussain4006w@users.noreply.github.com>
2023-10-14 11:14:38 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+  localStorage.setItem("player1_name", p1);
+	  //Set "player2_name" using localStorage.setItem() function
+    localStorage.setItem("player2_name", p2);
+    window.location = "game_page.html";
+}
+
